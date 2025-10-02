ffcaafea936b2fc9fc9440038885224c4e591a86
Till Grallert <till.grallert@fu-berlin.de>
2025-10-02 22:18:22 +0200

    added bash scripts for converting MARCXML to TEI/XML

---
+++ src/convert_marcxml-to-tei_folder.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# change into the script directory
+current_dir=$(dirname "${BASH_SOURCE[0]}")
+cd $current_dir && pwd
+# set a root directory for the repository, relative to this script
+root_dir=".."
+# set input directory relative to current directory
+input_dir="/Users/Shared/BachUni/BachBibliothek/GitHub/Sihafa/sihafa_data/bibliographic-data/sources/nloi/marcxml"
+output_dir=$input_dir
+# path to XSLT stylesheet
+xslt_file="xslt/convert_marc-xml-to-tei_file.xsl"
+cd $root_dir # change into root directory
+echo "Convert all MARC/XML files in $input_dir directory and convert them to TEI/XML"
+for file in $input_dir/*.mrcx # iterate over all files in the input directory
+do
+    echo "Applying XSLT to $file"
+    saxon -s:"$file" -xsl:$xslt_file \
+    p_stand-alone=false p_verbose=false p_debug=false \
+    p_output-folder="$output_dir/_output/"
+done
